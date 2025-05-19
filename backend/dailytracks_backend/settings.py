976316238e819792e156b41cd22272ecc893e973
Hosoda Keiji <kei.2001.1031@gmail.com>
2025-05-19 23:25:25 +0900

    ユーザー取得APIの作成

--- backend/dailytracks_backend/settings.py
+++ backend/dailytracks_backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'accounts',
+    "dailytracks",
 ]
 
 REST_FRAMEWORK = {
--- frontend/src/app/(auth)/home/page.tsx
+++ frontend/src/app/(auth)/home/page.tsx
@@ -1,10 +1,32 @@
-// frontend/src/app/home/page.tsx
 'use client'
+import { useEffect, useState } from "react";
+
 export default function HomePage() {
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) return;
+
+        fetch("http://localhost:8000/api/user/me/", {
+            headers: {
+                "Authorization": `Token ${token}`,
+            },
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.username) {
+                setUsername(data.username);
+            }
+        })
+        .catch((err) => console.error("ユーザー取得エラー:", err));
+    }, []);
+
     return (
         <div style={{ padding: '2rem' }}>
-        <h1>ログイン成功！</h1>
-        <p>ようこそ、ホーム画面へ。</p>
+            <h1>{username ? `${username} さんの日々の軌跡` : "ホーム"}</h1>
+            <p>ようこそ、ホーム画面へ。</p>
         </div>
-    )
+    );
 }
